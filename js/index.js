26a5be4cc52dbfe635400751038ac032401134a0
孙源玲 <3442153944@qq.com>
2024-03-25 15:19:00 +0800

    1

--- js/index.js
+++ js/index.js
@@ -5,13 +5,6 @@ function scrollTabs(distance) {
   function scrollTabs1(distance) {
     const tabBar = document.querySelector('.follow_user_works');
     tabBar.scrollBy({ left: distance, behavior: 'smooth' });
-  }
+}
 
-function scrollLeft() {  
-    const container = document.querySelector('.follow_user_works');  
-    container.scrollLeft -= 200;
-}
-function scrollRight() {  
-    const container = document.querySelector('.follow_user_works');  
-    container.scrollLeft += 200; 
-}
+
