882c038bb39bdb3d2f3bc6e1e7c6663679e6f5f6
孙源玲 <3442153944@qq.com>
2024-04-11 18:20:03 +0800

    开始换用vue加jQuery框架，新增用户中心

--- js/user_center.js
+++ js/user_center.js
@@ -1,12 +1,23 @@
-var data={msg1:"test1",msg2:"test2",msg3:"test3"};
-var vm=new Vue({
-    el:".test",
-    data:data
+
+
+var vm=new Vue ({
+    el:'.main_content',
+    data:{
+        edit_box_background_color_show:false,
+    },
+    computed:{
+        
+    },
+    methods:{
+        edit_btn:function()
+        {
+            console.log('test');
+            this.edit_box_background_color_show=true;
+        },
+        hidden_btn:function()
+        {
+            this.edit_box_background_color_show=false;
+        }
+    }
 })
-document.write(vm.msg1===data.msg1);
-document.write("<br>");
-vm.msg2="测试二";
-document.write(vm.msg2);
-document.write("<br>");
-data.msg3="测试三"
-document.write(vm.msg3);
+
