7547f4926666fca02134f3f418c2e4b3c16ed468
孙源玲 <3442153944@qq.com>
2024-04-11 19:19:09 +0800

    完成了些功能

--- js/user_center.js
+++ js/user_center.js
@@ -17,6 +17,21 @@ var vm=new Vue ({
         hidden_btn:function()
         {
             this.edit_box_background_color_show=false;
+        },
+        delete_btn:function()
+        {
+            if(window.confirm('是否删除该图片？')){
+                console.log("删除确认");
+            }
+            else{
+                console.log("删除取消");
+            }
+        },
+        done_upload:function(){
+            console.log("上传完成");
+        },
+        upload_img:function(){
+            console.log("选择完成");
         }
     }
 })
